2c3d34f21f1a6bd5480c20c4fa3bdcac10a7ac04
Carlos Toranzo <charly1020@gmail.com>
2014-11-30 16:46:29 -0300

    variable name changed

--- src/com/git/Hello/Main.java
+++ src/com/git/Hello/Main.java
@@ -5,12 +5,12 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Saludador sal = new Saludador();
-		Puteador putead = new Puteador();
+		Saludador saludador = new Saludador();
+		Puteador puteador = new Puteador();
 		
-		System.out.println(sal.sayHello("Robertito"));
-		System.out.println(putead.sayPuta("Sabrinita"));
-		System.out.println(putead.sayChoto("Juanito"));
+		System.out.println(saludador.sayHello("Robertito"));
+		System.out.println(puteador.sayPuta("Sabrinita"));
+		System.out.println(puteador.sayChoto("Juanito"));
 
 	}
 
